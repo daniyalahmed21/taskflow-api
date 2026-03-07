234d31bd1d61afc6e0d6ee2cf1b11345c813802b
Daniyal Ahmed <da9033431@gmail.com>
2026-03-07 13:10:10 +0000

    adding global caching

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { CacheInterceptor } from './common/interceptors/cache.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(app.get(CacheInterceptor))
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true,            //removes extra fields
