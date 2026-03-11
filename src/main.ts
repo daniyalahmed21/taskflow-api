34d90fb8b8635725fb98969bc919ef72e5af81ae
Daniyal Ahmed <da9033431@gmail.com>
2026-03-11 13:33:58 +0000

    learn microservice architecture

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
-import { CacheInterceptor } from './common/interceptors/cache.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'taskflow-service',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'taskflow-consumer',
+      },
+    },
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalInterceptors(app.get(CacheInterceptor))
-  app.useGlobalPipes(new ValidationPipe(
-    {
-      whitelist: true,            //removes extra fields
-      forbidNonWhitelisted: true, //throws error if extra fields are sent
-      transform: true,            //converts payload to expected type
-    }
-  ))
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen();
 }
+
 bootstrap();
