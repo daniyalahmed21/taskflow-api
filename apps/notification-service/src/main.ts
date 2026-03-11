145b1a2512b84e1101399826d387e27d066ba324
Daniyal Ahmed <da9033431@gmail.com>
2026-03-11 14:07:43 +0000

    setting up notification-service consumer

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice(NotificationServiceModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'notification-service',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'notification-consumer',
+      },
+    },
+  });
+  await app.listen();
 }
+
 bootstrap();
--- apps/notification-service/src/notification-service.controller.ts
+++ apps/notification-service/src/notification-service.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { NotificationServiceService } from './notification-service.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
-export class NotificationServiceController {
-  constructor(private readonly notificationServiceService: NotificationServiceService) {}
+export class NotificationConsumerController {
 
-  @Get()
-  getHello(): string {
-    return this.notificationServiceService.getHello();
+  @EventPattern('user_created')
+  handleUserCreated(@Payload() data: any) {
+    console.log('Send welcome email to:', data.email);
   }
 }
